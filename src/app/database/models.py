6ac3d8460807eee3eee69021bc31e369b473c1cc
Ashley Maher <ashley.maher@didymodesigns.com.au>
2024-12-27 13:03:27 +1100

    Fix import order with isort

--- src/app/database/models.py
+++ src/app/database/models.py
@@ -1,6 +1,5 @@
 from sqlalchemy import Column, Integer, String, create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 Base = declarative_base()
 
